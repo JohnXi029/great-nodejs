74151a9fb1790dc24d4a3281cc9443b2e0538618
xi <xiqiang@mingyoutech.com>
2018-03-03 14:37:47 +0800

    20180303

--- chapter2/v8-test.js
+++ chapter2/v8-test.js
@@ -4,12 +4,35 @@
  */
 
 // object#keys
-var a = {a: 'b', c: 'd'};
-for (var i in a) {
-console.log(i);
-console.log(a[i]);
-// hasOwnProperty()
-console.log(a.hasOwnProperty(i));  // true
-}
+// var a = {a: 'b', c: 'd'};
+// for (var i in a) {
+// console.log(i);
+// console.log(a[i]);
+// // hasOwnProperty()
+// console.log(a.hasOwnProperty(i));  // true
+// }
+// console.log(Object.keys(a));
 
-console.log(Object.keys(a));
+
+// array#isarray
+// console.log(Array.isArray(new Array));  // true
+// console.log(Array.isArray([]));  // true
+// console.log(Array.isArray(null));  // false
+// console.log(Array.isArray(arguments)); // false
+
+// 数组方法
+// [1, 2, 3].forEach((v) => {
+//   console.log(v);
+// });
+//
+// [1, 2, 3].forEach((v) => {
+//   return v < 3;
+// });  // 返回 [1, 2]
+//
+// // 改变数组的值
+// [1, 2, 3].forEach((v) => {
+//   return v*2;
+// });
+
+// 字符串方法
+// console.log('   hello   '.trim());
